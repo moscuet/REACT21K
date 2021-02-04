e9fd2645bf9a5f04fa3adef115094ed48509900e
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-02-04 04:30:04 +0200

    comleted cyclone_arcade

--- Javascript/Cyclone_Arcade/main.js
+++ Javascript/Cyclone_Arcade/main.js
@@ -1,9 +1,11 @@
 console.log('connected')
 
 let number_of_light = Number(document.getElementById('number_of_light').value)
-let time_delay = Number(document.getElementById('time_delay').value)
+let time_delay =  Number(document.getElementById('time_delay').value)
+let chosen_light_index = Number(document.getElementById('chosen_light').value)
 let gameOn = false
 let counter
+
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -22,7 +24,8 @@ let createLight = () =>{
     counter = 0;
     removeAllChildNodes(document.getElementById('bulbs'));
     number_of_light = Number(document.getElementById('number_of_light').value)||10
-     
+    chosen_light_index = Number(document.getElementById('chosen_light').value)
+    time_delay =  Number(document.getElementById('time_delay').value)
     let count= 0;
     while ( count <  number_of_light){
       let newEl = document.createElement('div')
@@ -30,7 +33,8 @@ let createLight = () =>{
       document.getElementById('bulbs').appendChild(newEl)
       count++
     }
-    // document.querySelectorAll('.bulb')[0].classList.add('active')
+    document.querySelectorAll('.bulb')[chosen_light_index].classList.add('chosen')
+    arrangeBulbsInACircle(document.getElementsByClassName('bulb'));
 }
 createLight()
 
@@ -38,13 +42,11 @@ createLight()
 //let interval =  setInterval ( timer,250)
 
 const lightOff = () =>{
-     if(counter === 0) document.getElementById('win').innerText = 'You have won!!'
-    console.log(document.getElementById('win'))
+     if(counter === chosen_light_index) document.getElementById('win').innerText = 'You have won!!'
     if(gameOn){
         clearInterval(interval)
         gameOn = false
     }
-
 }
 
 const lightOn = () =>{
@@ -58,11 +60,27 @@ const lightOn = () =>{
 }
 
 
-
-
 document.getElementById('stop').addEventListener('click',lightOff)
 document.getElementById('play').addEventListener('click',lightOn)
 
 
 
 
+function arrangeBulbsInACircle  (nodes)  {   
+    const radius = '12em',
+        start = -90,
+        $els = [...nodes], // turn nodelist into a real array
+        numberOfEls = $els.length,
+        slice = 360 / numberOfEls,
+        index = 0;
+
+    $els.forEach((el, index) => {
+        const rotate = slice * index + start;
+        const rotateReverse = rotate * (-1);
+
+        el.style.transform = `rotate(${rotate}deg) translate(${radius}) rotate(${rotateReverse}deg)`;
+    })
+}
+
+// invoke the fn on the class bulb elements to create the effect
+arrangeBulbsInACircle(document.getElementsByClassName('bulb'));
