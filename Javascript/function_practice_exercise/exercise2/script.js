160edfc59cde48fedf33f98cd7948511ba5fb2c8
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-02-21 13:21:10 +0200

    function exercise 2 done

--- Javascript/function_practice_exercise/exercise2/script.js
+++ Javascript/function_practice_exercise/exercise2/script.js
@@ -1,54 +1,8 @@
 (()=>{
-  const executeScript = (studentNo,name,age) => {
-    const table = document.getElementById('table')
-    let row = document.createElement('tr');
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    let td4 = document.createElement('td');
-    td1.innerHTML = studentNo;
-    td2.innerHTML = name;
-    td3.innerHTML = age;
-    td4.innerHTML = studentNo > 0 ? 'student ': 'stuff'
-    row.append(td1,td2,td3,td4)
-    table.appendChild(row)
- }
- 
- window.addEventListener('DOMContentLoaded', ()=> {
-    executeScript('2100999', 'TestUser', 70)
- });
-
-
  // exercise 2.1: 
 
- const getUsers = () => [
-   {
-     studentNo: 123,
-     name: 'harry',
-     age: 15
-   },
-   {
-     studentNo: 234,
-     name: 'Ron',
-     age: 16
-   },
-   {
-     studentNo: 135,
-     name: 'Hermione',
-     age: 17
-   },
-   {
-     studentNo: -1,
-     name: 'Snape',
-     age: 55
-   },
-   {
-     studentNo: -1,
-     name: 'Hagrid',
-     age: 65
-   }
- ];
-
+ users = getUsers()
+ users.push({ studentNo:11, name:'rah', age:5})
  // exercise 2.2: 
  
  const addUser = (user) => {
@@ -68,10 +22,7 @@
  }
  
  //2.3 
- let users = getUsers()
- users.forEach( user => {
-    addUser(user)
- })
+users.forEach( user => addUser(user) )
 
  // 2.4
 
@@ -85,67 +36,47 @@
 }
  
 //5 funtion to find  if student or stuff
+const thead = document.getElementById('thead')
+const th = document.createElement('th')
+th.innerHTML = 'is stuff ?'
+thead.appendChild(th)
 
-let isStuff =  user => user.studentNo <0
-
-console.log('oldest',getOldest(users))
 
 
+// 6
 
-// 7
+  const userByAgeDec = (arr) =>{
+      for (let i = 0; i < arr.length; i++) {
 
-const userByAgeDec = (arr) =>{
-    console.log(arr)
-    for (let i = 0; i < arr.length; i++) {
+          for (let j = i + 1; j < arr.length; j++) {
+              let small  = 0
+              if (arr[i].age < arr[j].age) {
+                  small= arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = small;
+              }
+          }
+    }
+    return arr
+  }
 
-        for (let j = i + 1; j < arr.length; j++) {
-            let small  = 0
-
-            if (arr[i].age < arr[j].age) {
-
-                small= arr[i];
-
-                arr[i] = arr[j];
-
-                arr[j] = small;
-
-            }
-
-        }
-   }
-   return arr
-}
-
-console.log('ooo',userByAgeDec(users))
- 
- 
- 
- 
- 
- 
- 
+  let sortedtable = () =>{
+    const table = document.getElementById('table')
+    let sortedUsers = userByAgeDec([...users])
+    table.innerHTML = `<tr id = 'thead'>
+                          <th>Student No</td>
+                          <th>name</td>
+                          <th>age</td>
+                          <th>is stuff ?</td>
+                      </tr>`
+      sortedUsers.forEach( user => {
+      addUser(user)
+  })
+  }
+  document.getElementById('sortButton').addEventListener('click', ()=>{
+    sortedtable()
+  })
 
 })()
 
 
-
-/**
- * 
- * 
- * const executeScript = (id,name,age) => {
-    const table = document.getElementById('table')
-    let row = document.createElement('tr');
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    td1.innerHTML = id;
-    td2.innerHTML = name;
-    td3.innerHTML = age;
-    row.append(td1,td2,td3)
-    table.appendChild(row)
- }
- 
- window.addEventListener('DOMContentLoaded', ()=> {
-    executeScript('s2100999', 'TestUser', 70)
- });
- */
