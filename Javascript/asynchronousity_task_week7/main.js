4e4f2404dc889902403c1eb22ad71c2f517811b5
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-03-07 23:14:48 +0200

    pokedex code refactored

--- Javascript/asynchronousity_task_week7/main.js
+++ Javascript/asynchronousity_task_week7/main.js
@@ -8,18 +8,18 @@ fetch(url)
     let ul = document.createElement('ul')
     data.results.forEach( (d)=> {
         let li = document.createElement('li')
-        li.innerHTML = `<a href = '#' onClick = "pokeMan('${d.name}','${d.url}')">${d.name}</a>`
+        li.innerHTML = `<a href = '#' onClick = "pokeMan('${d.url}')">${d.name}</a>`
         ul.appendChild(li)
     })
     document.getElementById('results').appendChild(ul)
 })
 
-function pokeMan (name,url) {
+function pokeMan (url) {
     
     fetch(url)
         .then ( response => response.json())
         .then (data => {
-            let {base_experience, height, id, order, weight, abilities, forms, species, types} = data
+            let {name,base_experience, height, id, order, weight, abilities, forms, species, types} = data
            document.getElementById('results').innerHTML = `
            <div><img src="${data.sprites.front_default}"> </div>
            <h3>Name : ${name}</h3>
@@ -29,7 +29,7 @@ function pokeMan (name,url) {
             <li> Base experience : ${base_experience} </li>
             <li>Height : ${height}</li>
             <li> Weight: ${weight} </li>
-            <li> Species: ${species} </li>
+            <li> Species: ${species.name} </li>
             <li> Abilities: <ul> ${abilities.map( a => `<li> ${a.ability.name}</li>`).join('')}   </li> </ul>
             <li> Forms: <ul> ${forms.map( form => `<li> ${form.name}</li>`).join('')}</li> </ul>
             <li> Types: <ul> ${types.map( t => `<li> ${t.type.name}</li>`).join('')}</li> </ul>
