ef4a6aa0e32cc3b608283bd10fe42e7a48b2c0cd
Mostafizur Rahman <51766137+moscuet@users.noreply.github.com>
2021-03-30 10:04:31 +0300

    Update index.php

--- PHP/day1/number_counter/index.php
+++ PHP/day1/number_counter/index.php
@@ -41,47 +41,47 @@
                 break;
             default:
         }
-    }  else { 
-            switch($part2){
-                case 2 : $result= 'kaksikymmentä';
-                    break;
-                case 3 : $result= 'kolmekymmentä';
-                    break;
-                case 4 : $result= 'neljakymmentä';
-                    break;
-                case 5 : $result= 'viisikymmentä';
-                    break;
-                case 6 : $result= 'kuusikymmentä';
-                    break;
-                case 7 : $result= 'seitsemänkymmentä';
-                    break;
-                case 8 : $result= 'kahdeksankymmentä';
-                    break;
-                case 9 : $result= 'yhdeksänkymmentä';
-                    break;
-                    default: $result = '';
+    } else { 
+        switch($part2){
+            case 2 : $result= 'kaksikymmentä';
+                break;
+            case 3 : $result= 'kolmekymmentä';
+                break;
+            case 4 : $result= 'neljakymmentä';
+                break;
+            case 5 : $result= 'viisikymmentä';
+                break;
+            case 6 : $result= 'kuusikymmentä';
+                break;
+            case 7 : $result= 'seitsemänkymmentä';
+                break;
+            case 8 : $result= 'kahdeksankymmentä';
+                break;
+            case 9 : $result= 'yhdeksänkymmentä';
+                break;
+                default: $result = '';
+        }
+        switch($part1){
+            case 1 : $result.= 'yksi';
+                break;
+            case 2 : $result .= 'kaksi';
+                break;
+            case 3 : $result .= 'kolme';
+                break;
+            case 4 : $result .= 'nelja';
+                break;
+            case 5 : $result .= 'viisi';
+                break;
+            case 6 : $result .= 'kuusi';
+                break;
+            case 7 : $result .= 'seitsemän';
+                break;
+            case 8 : $result .= 'kahdeksan';
+                break;
+            case 9 : $result .= 'yhdeksän';
+                break;
+            default: $result .= '';
             }
-            switch($part1){
-                case 1 : $result.= 'yksi';
-                    break;
-                case 2 : $result .= 'kaksi';
-                    break;
-                case 3 : $result .= 'kolme';
-                    break;
-                case 4 : $result .= 'nelja';
-                    break;
-                case 5 : $result .= 'viisi';
-                    break;
-                case 6 : $result .= 'kuusi';
-                    break;
-                case 7 : $result .= 'seitsemän';
-                    break;
-                case 8 : $result .= 'kahdeksan';
-                    break;
-                case 9 : $result .= 'yhdeksän';
-                    break;
-                default: $result .= '';
-                }
     }
     echo "NUmber in Finnish: $result"
 
