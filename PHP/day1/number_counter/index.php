4e2195def29601f4a0bf60333907348921fa008f
Mostafizur Rahman <51766137+moscuet@users.noreply.github.com>
2021-03-30 10:05:35 +0300

    Update index.php

--- PHP/day1/number_counter/index.php
+++ PHP/day1/number_counter/index.php
@@ -9,82 +9,79 @@
 <body>
     <h1>Number counting</h1>
     <?php 
-      $randomNumber = rand(0,100);
-    //   $part1Text = '';
-    //   $part2Text= '';
-    $part1 = $randomNumber%10;
-    $part2 = floor($randomNumber/10);
-    $numberText ='';
-    $result ='';
-    echo "The number is: $randomNumber<br>";
-    if($part2===1){
-        switch($part1){
-            case 0 : $result= 'kymmenen';
-            break;
-            case 1 : $result= 'yksitoista';
-            break;
-            case 2 : $result= 'kaksitoista';
+        $randomNumber = rand(0,100);
+        $part1 = $randomNumber%10;
+        $part2 = floor($randomNumber/10);
+        $numberText ='';
+        $result ='';
+        echo "The number is: $randomNumber<br>";
+        if($part2===1){
+            switch($part1){
+                case 0 : $result= 'kymmenen';
                 break;
-            case 3 : $result= 'kolmetoista';
+                case 1 : $result= 'yksitoista';
                 break;
-            case 4 : $result= 'neljatoista';
-                break;
-            case 5 : $result= 'viisitoista';
-                break;
-            case 6 : $result= 'kuusitoista';
-                break;
-            case 7 : $result= 'seitsemäntoista';
-                break;
-            case 8 : $result= 'kahdeksantoista';
-                break;
-            case 9 : $result= 'yhdeksäntoista';
-                break;
-            default:
-        }
-    } else { 
-        switch($part2){
-            case 2 : $result= 'kaksikymmentä';
-                break;
-            case 3 : $result= 'kolmekymmentä';
-                break;
-            case 4 : $result= 'neljakymmentä';
-                break;
-            case 5 : $result= 'viisikymmentä';
-                break;
-            case 6 : $result= 'kuusikymmentä';
-                break;
-            case 7 : $result= 'seitsemänkymmentä';
-                break;
-            case 8 : $result= 'kahdeksankymmentä';
-                break;
-            case 9 : $result= 'yhdeksänkymmentä';
-                break;
-                default: $result = '';
-        }
-        switch($part1){
-            case 1 : $result.= 'yksi';
-                break;
-            case 2 : $result .= 'kaksi';
-                break;
-            case 3 : $result .= 'kolme';
-                break;
-            case 4 : $result .= 'nelja';
-                break;
-            case 5 : $result .= 'viisi';
-                break;
-            case 6 : $result .= 'kuusi';
-                break;
-            case 7 : $result .= 'seitsemän';
-                break;
-            case 8 : $result .= 'kahdeksan';
-                break;
-            case 9 : $result .= 'yhdeksän';
-                break;
-            default: $result .= '';
+                case 2 : $result= 'kaksitoista';
+                    break;
+                case 3 : $result= 'kolmetoista';
+                    break;
+                case 4 : $result= 'neljatoista';
+                    break;
+                case 5 : $result= 'viisitoista';
+                    break;
+                case 6 : $result= 'kuusitoista';
+                    break;
+                case 7 : $result= 'seitsemäntoista';
+                    break;
+                case 8 : $result= 'kahdeksantoista';
+                    break;
+                case 9 : $result= 'yhdeksäntoista';
+                    break;
+                default:
             }
-    }
-    echo "NUmber in Finnish: $result"
-
+        } else { 
+            switch($part2){
+                case 2 : $result= 'kaksikymmentä';
+                    break;
+                case 3 : $result= 'kolmekymmentä';
+                    break;
+                case 4 : $result= 'neljakymmentä';
+                    break;
+                case 5 : $result= 'viisikymmentä';
+                    break;
+                case 6 : $result= 'kuusikymmentä';
+                    break;
+                case 7 : $result= 'seitsemänkymmentä';
+                    break;
+                case 8 : $result= 'kahdeksankymmentä';
+                    break;
+                case 9 : $result= 'yhdeksänkymmentä';
+                    break;
+                    default: $result = '';
+            }
+            switch($part1){
+                case 1 : $result.= 'yksi';
+                    break;
+                case 2 : $result .= 'kaksi';
+                    break;
+                case 3 : $result .= 'kolme';
+                    break;
+                case 4 : $result .= 'nelja';
+                    break;
+                case 5 : $result .= 'viisi';
+                    break;
+                case 6 : $result .= 'kuusi';
+                    break;
+                case 7 : $result .= 'seitsemän';
+                    break;
+                case 8 : $result .= 'kahdeksan';
+                    break;
+                case 9 : $result .= 'yhdeksän';
+                    break;
+                default: $result .= '';
+                }
+        }
+        echo "NUmber in Finnish: $result"
     ?>
 </body>
 </html>
