fb836692bf4297c0de939cb94f26c5dd75971bf3
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-04-06 09:00:38 +0300

    displaying pokomon according pagenumber requested by user

--- PHP/Handle_Pokemons/index.php
+++ PHP/Handle_Pokemons/index.php
@@ -13,7 +13,7 @@
     $parsed_data = json_decode($data, true);
    
     $parsed_data['next'] = 2;
-    $parsed_data['previous'] = 3;
+    $parsed_data['previous'] = 0;
     $next = $parsed_data['next'];
     $previous = $parsed_data['previous'];
     $pokemonsArray = $parsed_data['results'];
@@ -24,20 +24,27 @@
         </div>"
 );  
 
-// looping through pokomens array
-   
+// getting page number from Request ; default page number is 1
+$page = 1;
+if($_GET['page']) $page = $_GET['page'];
+echo $page;
+
+  
    $chunkPokemon = array_chunk($pokemonsArray, 50, true); 
-   $chunkPokemonToPrint = array_slice($chunkPokemon,2,3);
-    
+   $chunkPokemonToPrint = array_slice( $chunkPokemon, $page, $page+1);
+   
+   // looping through chunkPokemon array
+
    foreach($chunkPokemonToPrint[0] as $key => $value ){
         echo '<br>';
         print_r($key. '. ' . $value['name']. ': '.$value['url']);
     };
 
 
-    echo "<a href='/index.php?page=$next'>Next</a>";
-    echo 'page'.$_REQUEST['page'];
+    // echo "<a href='/index.php?page=$next'>Next</a>";
+    // echo 'page'.$_REQUEST['page'];
 
+   
     ?>
 </body>
 </html>
